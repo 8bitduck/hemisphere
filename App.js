4b0efb578104ad490d59cfea19cc8604fd543df7
Bryan Rehbein <bryan@siliconsea.net>
2018-03-07 16:21:57 -0600

    Initial Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, WebView } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WebView
+        style={styles.container}
+        source={{uri: 'https://www.hemispheredm.com'}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40
+  },
+});
